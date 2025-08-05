a1cd86bcc56c9600726bbd004696a612ef4320ce
nethuwh-xmd <nethumjayarathna8@gmail.com>
2025-08-05 14:48:50 +0530

    Update main-alive.js

--- Plugin/main-alive.js
+++ Plugin/main-alive.js
@@ -39,8 +39,8 @@ lite({
                 forwardingScore: 1000,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363402507750390@newsletter',
-                    newsletterName: 'Malvin Tech',
+                    newsletterJid: '@newsletter',
+                    newsletterName: '',
                     serverMessageId: 143
                 }
             }
